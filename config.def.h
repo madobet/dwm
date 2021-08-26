5812e0017ff77de0ec91347b9b63313f67fc989c
madobet <madobet@outlook.com>
2021-08-26 13:21:23 +0800

    Modified config and applied systray plugin

--- config.def.h
+++ config.def.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka Term:size=10", "monospace:size=10" };
@@ -81,17 +86,20 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() dmenu 的显示器输出？*/
-static const char *dmenucmd[]   = { "cmdmenu", NULL };
-static const char *dmenuapp[]   = { "appmenu", NULL };
+// static const char *dmenucmd[]   = { "dmenu_path" ,"|" ,"dmenu" ,"-b" ,"-f" ,"-i" ,"-l" ,"18" ,"-p" ,"" ,"-fn" ,"monospace-9:Medium" ,"-bd" ,"1" ,"-w" ,"618" ,"-c" ,"-F" ,"-H" ,"${XDG_CACHE_HOME:-~/.cache}/runmenu.history" ,"|" ,"${SHELL:-'/bin/sh'}" ,"&" , NULL};
+static const char *dmenucmd[]   = { "dmenu_path" ,"|" ,"dmenu" ,"-b" ,"-f" ,"-i" ,"-l" ,"18" ,"-p" ,"" ,"-fn" ,"monospace-9:Medium" ,"-bd" ,"1" ,"-w" ,"618" ,"-c" ,"-F" ,"-H" ,"${XDG_CACHE_HOME:-~/.cache}/runmenu.history" , NULL};
+static const char *dmenuapp[]   = { "rofi", "-columns", "2", "-show-icons", "-icon-theme", "Papirus", "-no-lazy-grab", "-show", "drun", "-theme", "themes/appsmenu.rasi", NULL };
 static const char *dclipboard[] = { "clipboard", NULL };
-static const char *termmain[]   = { "start-term", "main", "float", NULL };
-static const char *termbakup[]  = { "start-term", "kitty", "float", NULL };
+static const char *copyqboard[] = { "copyq", "toggle", NULL };
+static const char *termmain[]   = { "xterm", NULL };
+static const char *termbakup[]  = { "urxvt", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = dmenuapp } },
-  { MODKEY,                       XK_c,      spawn,          {.v = dclipboard }},
+	{ MODKEY,                       XK_s,      spawn,          {.v = dmenuapp } },
+    { MODKEY,                       XK_c,      spawn,          {.v = dclipboard }},
+    { MODKEY,                       XK_q,      spawn,          {.v = copyqboard }},
 	{ MODKEY,                       XK_Return, spawn,          {.v = termmain } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termbakup } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -101,7 +109,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_u,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_i,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask|ControlMask,             XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask|ControlMask, XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -143,4 +151,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
